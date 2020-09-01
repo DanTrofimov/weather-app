afb98d4c5b2f2c6868fea51689bc1c143a62acae
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-09-01 23:58:39 +0300

    added native theme chnger with using CSS-variables

---
+++ weather-app/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+    <div id="app">
+        <HeaderBar @changeTheme="darkThemeSwitch"/>
+    </div>
+</template>
+
+<script>
+
+import HeaderBar from "./components/HeaderBar";
+export default {
+    name: 'App',
+    components: {
+        HeaderBar
+    },
+    methods: {
+        _addDarkTheme() {
+            let darkThemeLinkEl = document.createElement("link");
+            darkThemeLinkEl.setAttribute("rel", "stylesheet");
+            darkThemeLinkEl.setAttribute("href", "css/dark-theme.css");
+            darkThemeLinkEl.setAttribute("id", "dark-theme-style");
+
+            let docHead = document.querySelector("head");
+            docHead.append(darkThemeLinkEl);
+        },
+        _addLightTheme() {
+            let darkThemeLinkEl = document.querySelector("#dark-theme-style");
+            let parentNode = darkThemeLinkEl.parentNode;
+            parentNode.removeChild(darkThemeLinkEl);
+
+            let lightThemeLinkEl = document.createElement("link");
+            darkThemeLinkEl.setAttribute("rel", "stylesheet");
+            darkThemeLinkEl.setAttribute("href", "css/light-theme.css");
+            darkThemeLinkEl.setAttribute("id", "light-theme-style");
+
+            let docHead = document.querySelector("head");
+            docHead.append(lightThemeLinkEl);
+        },
+        darkThemeSwitch() {
+            let darkThemeLinkEl = document.querySelector("#dark-theme-style");
+            if (!darkThemeLinkEl) {
+                this._addDarkTheme()
+            } else {
+                this._addLightTheme()
+            }
+        }
+    },
+}
+</script>
+
+<style>
+
+/*theme colors*/
+
+:root {
+    --background-color_light: white;
+    --background-color_dark: black;
+
+    --text-color_light: black;
+    --text-color_dark: white;
+}
+
+* {
+      font-family: Consolas, sans-serif;
+      margin: 0;
+      padding: 0;
+}
+
+#app {
+      transition: all 0.5s;
+      max-width: 800px;
+      margin: auto;
+}
+
+</style>
---
+++ weather-app/src/components/HeaderBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header-wrapper">
+        <p class="header-wrapper__logo">
+            weather-app
+        </p>
+        <p class="header-wrapper__theme-changer" v-on:click="$emit('changeTheme')">
+            change theme
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeaderBar",
+    }
+</script>
+
+<style scoped>
+    .header-wrapper {
+        display: flex;
+        justify-content: space-between;
+        height: 30px;
+        margin-top: 20px;
+    }
+
+    .header-wrapper__logo {
+        font-size: 20px;
+        font-weight: bold;
+    }
+
+    .header-wrapper__theme-changer {
+        padding: 5px;
+        background-color: rgba(255, 255, 255, 0);
+    }
+</style>
