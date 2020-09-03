12420453b920a09e24d460992cd6a11e6d5162bb
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-09-03 19:19:25 +0300

    added custom weather search

--- weather-app/src/store/modules/weather.js
+++ weather-app/src/store/modules/weather.js
@@ -5,19 +5,32 @@ export default {
             const weatherResponse = await fetch(`${process.env.VUE_APP_BASE_URL}group?id=${cities}&units=metric&APPID=${process.env.VUE_APP_API_KEY}`);
             const weatherResult = await weatherResponse.json();
             ctx.commit('updateWeather', weatherResult)
+        },
+
+        async fetchWeatherByName(ctx, name) {
+            const customWeatherResponse = await fetch(`${process.env.VUE_APP_BASE_URL}weather?q=${name}&units=metric&APPID=${process.env.VUE_APP_API_KEY}`);
+            const customWeather = await customWeatherResponse.json();
+            ctx.commit('updateCustomWeather', customWeather)
         }
     },
     mutations: {
         updateWeather(state, weather) {
             state.weather = weather
+        },
+        updateCustomWeather(state, customWeather) {
+            state.customWeather = customWeather
         }
     },
     state: {
         weather: [],
+        customWeather: {},
     },
     getters: {
         getWeather(state) {
             return state.weather;
         },
+        getCustomWeather(state) {
+            return state.customWeather;
+        }
     }
 }
--- weather-app/src/views/DefaultWeather.vue
+++ weather-app/src/views/DefaultWeather.vue
@@ -11,6 +11,29 @@
         <p class="obtained-info">
             Obtained {{ getObtainedDate }}
         </p>
+        <h3 class="suggestion">
+            Try to find out weather forecast for your city:
+        </h3>
+        <div class="search">
+            <label>
+                <input
+                        type="text"
+                        class="search__bar"
+                        placeholder="Enter the city name..."
+                        v-model = "query"
+                        @keypress= "submit"
+                >
+            </label>
+            <button class="search__icon"
+                    @click="loadCustomWeather"
+            >
+                🔍
+            </button>
+        </div>
+        <WeatherItem
+                v-if="typeof getCustomWeather != 'undefined'"
+                v-bind:weather="getCustomWeather"
+        />
     </div>
 </template>
 
@@ -24,13 +47,30 @@
             getWeather() {
                 return this.$store.getters.getWeather;
             },
+            getCustomWeather() {
+                return this.$store.getters.getCustomWeather;
+            },
             getObtainedDate() {
                 return new Date(this.getWeather.list[0].dt*1000).toLocaleString("en-US")
             }
         },
-        methods: mapActions(['fetchWeather']),
+        methods: {
+             ...mapActions(['fetchWeather', 'fetchWeatherByName']),
+            loadCustomWeather() {
+                console.log(this.getCustomWeather);
+                if (typeof this.getCustomWeather != 'undefined') {
+                    this.fetchWeatherByName(this.query)
+                }
+            },
+            submit(e) {
+                if (e.key === "Enter" && this.query.trim()) {
+                    this.loadCustomWeather();
+                }
+            }
+        },
         data() {
             return {
+                query: '',
                 imgLink: process.env.VUE_APP_GET_IMG_URL,
                 cities: [
                     {name : 'Boston', id : 4930956},
@@ -64,9 +104,51 @@
         margin: 30px auto;
     }
 
+    .suggestion {
+        font-size: 1.5em;
+        text-align: center;
+        padding: 0 30px 0 30px;
+        margin-bottom: 20px;
+    }
+
+    .search {
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+        margin-bottom: 100px;
+        padding: 0 50px 0 70px;
+    }
+
+    .search__bar {
+        width: 550px;
+        height: 30px;
+        padding: 0 10px 0 10px;
+    }
+
+    .search__icon {
+        font-size: 20px;
+        border-radius: 10px;
+        background-color: rgb(0,0,0,0);
+        border: none;
+    }
+
     @media screen and (max-width: 490px) {
+        .default-wrapper {
+            grid-template-columns: repeat(auto-fill, 170px);
+        }
+
         .obtained-info {
             width: 210px;
+        }
+
+        .suggestion {
+            font-size: 1.2em;
+        }
+    }
+
+    @media screen and (max-width: 410px) {
+        .default-wrapper {
+            grid-template-columns: repeat(auto-fill, 230px);
         }
     }
 </style>
